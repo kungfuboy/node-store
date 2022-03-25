45e6dc75a42432657060fdc97c48be78e88ffdcb
夜鹰 <17kungfuboy@gmail.com>
2022-03-25 19:35:58 +0800

    update

--- app.js
+++ app.js
@@ -1,54 +1,69 @@
-const Koa = require('koa')
-const json = require('koa-json')
-const Router = require('koa-router')
-const fs = require('fs')
-const path = require('path')
-const bodyParser = require('koa-bodyparser')
-const cors = require('@koa/cors')
+const Koa = require("koa");
+const json = require("koa-json");
+const Router = require("koa-router");
+const fs = require("fs");
+const path = require("path");
+const bodyParser = require("koa-bodyparser");
+const cors = require("@koa/cors");
 
-const app = new Koa()
-const router = new Router()
+const app = new Koa();
+const router = new Router();
 
-app.use(cors())
+// CORS middleware
+app.use(cors());
 // JSON prettier middleware
-app.use(json())
+app.use(json());
 // Bodyparser middleware
-app.use(bodyParser())
+app.use(bodyParser());
 
 // app.use(async ctx => ctx.body = {msg: 'hello'})
 
-const errorBody = (code, msg) => ({ code, msg })
+const responseBody = (code, msg) => ({ code, msg });
 
-router.get('/view', async (ctx) => {
-  const json = fs.readFileSync('./data.json', 'utf8')
-  ctx.body = json
-})
+router.get("/view", async (ctx) => {
+  const json = fs.readFileSync("./data.json", "utf8");
+  ctx.body = json;
+});
 
-router.post('/upload', (ctx) => {
-  const json = fs.readFileSync('./data.json', 'utf8')
-  const { name, version, author, main, ...it } = ctx.request.body
+router.post("/upload", (ctx) => {
+  const json = fs.readFileSync("./data.json", "utf8");
+  const { name, version, author, main, ...it } = ctx.request.body;
   if (!name) {
-    ctx.request.body = errorBody(-1, `You need sumbit 'name' data.`)
+    ctx.request.body = responseBody(-1, `You need sumbit 'name' data.`);
   }
   if (!version) {
-    ctx.request.body = errorBody(-1, `You need sumbit 'version' data.`)
+    ctx.request.body = responseBody(-1, `You need sumbit 'version' data.`);
   }
   if (!author) {
-    ctx.request.body = errorBody(-1, `You need sumbit 'author' data.`)
+    ctx.request.body = responseBody(-1, `You need sumbit 'author' data.`);
   }
   if (!main) {
-    ctx.request.body = errorBody(-1, `You need sumbit 'main' data.`)
+    ctx.request.body = responseBody(-1, `You need sumbit 'main' data.`);
   }
-  json.unreliable[name] = { name, version, author, main, ...it }
-  fs.writeFileSync('./data.json', json)
-  ctx.request.body = successBody(0, 'Save data success.')
-})
+  json.unreliable[name] = { name, version, author, main, ...it };
+  fs.writeFileSync("./data.json", json);
+  ctx.request.body = successBody(0, "Save data success.");
+});
 
-router.get('/data/:name', (ctx) => {
-  console.log(ctx.params.name)
-})
+router.put("/reliable/:name", (ctx) => {
+  const { name } = ctx.params;
+  const json = fs.readFileSync("./data.json", "utf8");
+  json.reliable[name] = json.unreliable[name];
+  delete json.unreliable[name];
+  fs.writeFileSync("./data.json", json);
+  ctx.request.body = responseBody(0, "Reliable success.");
+});
+
+router.put("/unreliable/:name", (ctx) => {
+  const { name } = ctx.params;
+  const json = fs.readFileSync("./data.json", "utf8");
+  json.unreliable[name] = json.reliable[name];
+  delete json.reliable[name];
+  fs.writeFileSync("./data.json", json);
+  ctx.request.body = responseBody(0, "Unreliable success.");
+});
 
 // Router middleware
-app.use(router.routes()).use(router.allowedMethods())
+app.use(router.routes()).use(router.allowedMethods());
 
-app.listen(3000, () => console.log('Server started...'))
+app.listen(3000, () => console.log("Server started..."));
