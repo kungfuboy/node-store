7295034e043fb6d82db5e8e6eb08c2b3b7f8766e
夜鹰 <17kungfuboy@gmail.com>
2022-04-07 20:31:41 +0800

    update

--- api/index.js
+++ api/index.js
@@ -1,24 +1,8 @@
 const Koa = require("koa");
-const json = require("koa-json");
 const Router = require("koa-router");
-const fs = require("fs");
-const path = require("path");
-const bodyParser = require("koa-bodyparser");
-const cors = require("@koa/cors");
 
 const app = new Koa();
 const router = new Router();
-
-// CORS middleware
-app.use(cors());
-// JSON prettier middleware
-app.use(json());
-// Bodyparser middleware
-app.use(bodyParser());
-
-// app.use(async ctx => ctx.body = {msg: 'hello'})
-
-const responseBody = (code, msg) => ({ code, msg });
 
 router.get("/", async (ctx) => {
   ctx.body = "A";
@@ -32,3 +16,5 @@ router.get("/b", async (ctx) => {
 app.use(router.routes()).use(router.allowedMethods());
 
 app.listen(3333, () => console.log("Server started..."));
+
+module.exports = app;
